a55b8bca904b39a81a836ccb93886e165f275760
rekakurniawan <reka.kurniawan7@gmail.com>
2020-11-17 21:51:39 +0700

    Add files via upload

---
+++ deb10.sh
@@ -0,0 +1,280 @@
+#!/bin/bash
+#
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=`curl -s ifconfig.me`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# detail nama perusahaan
+country=ID
+state=DKIJakarta
+locality=JakartaTimur
+organization=Skyla
+organizationalunit=Skyla
+commonname=Skyla
+email=-
+
+# go to root
+cd
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# set time GMT +7 jakarta
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
+# Edit file /etc/systemd/system/rc-local.service
+cat > /etc/systemd/system/rc-local.service <<-END
+[Unit]
+Description=/etc/rc.local
+ConditionPathExists=/etc/rc.local
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+END
+
+# nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+echo "=================  install neofetch  ===================="
+echo "========================================================="
+# install neofetch
+apt-get update -y
+apt-get -y install gcc
+apt-get -y install make
+apt-get -y install cmake
+apt-get -y install git
+apt-get -y install screen
+apt-get -y install unzip
+apt-get -y install curl
+git clone https://github.com/dylanaraps/neofetch
+cd neofetch
+make install
+make PREFIX=/usr/local install
+make PREFIX=/boot/home/config/non-packaged install
+make -i install
+apt-get -y install neofetch
+cd
+echo "clear" >> .bashrc
+echo "neofetch" >> .bashrc
+
+# update
+apt-get -y update
+
+# set repo webmin
+sh -c 'echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list'
+wget -qO - http://www.webmin.com/jcameron-key.asc | apt-key add -
+
+echo "================  install Dropbear ======================"
+echo "========================================================="
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 110"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+
+echo "=================  install Squid3  ======================"
+echo "========================================================="
+
+# setting dan install vnstat debian 9 64bit
+apt-get -y install vnstat
+systemctl start vnstat
+systemctl enable vnstat
+chkconfig vnstat on
+chown -R vnstat:vnstat /var/lib/vnstat
+
+# install squid3
+cd
+apt-get -y install squid3
+wget -O /etc/squid/squid.conf "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf;
+/etc/init.d/squid restart
+
+# install webmin
+cd
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.962_all.deb
+dpkg --install webmin_1.962_all.deb;
+apt-get -y -f install;
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm -f webmin_1.910_all.deb
+/etc/init.d/webmin restart
+
+echo "=================  install stunnel  ====================="
+echo "========================================================="
+
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[dropbear]
+accept = 443
+connect = 127.0.0.1:143
+[dropbear]
+accept = 80
+connect = 127.0.0.1:110
+[openvpnssl]
+accept = 833
+connect = 127.0.0.1:1194
+
+END
+
+echo "=================  membuat Sertifikat OpenSSL ======================"
+echo "========================================================="
+#membuat sertifikat
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+# konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart
+
+echo "================= Auto Installer Disable badVPN V 3  ======================"
+# buat directory badvpn
+cd /usr/bin
+mkdir build
+cd build
+wget https://github.com/ambrop72/badvpn/archive/1.999.130.tar.gz
+tar xvzf 1.999.130.tar.gz
+cd badvpn-1.999.130
+cmake -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_TUN2SOCKS=1 -DBUILD_UDPGW=1
+make install
+make -i install
+
+# auto start badvpn single port
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/null &' /etc/rc.local
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/null &
+cd
+
+# auto start badvpn second port
+cd /usr/bin/build/badvpn-1.999.130
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/null &' /etc/rc.local
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/null &
+cd
+
+# auto start badvpn second port
+cd /usr/bin/build/badvpn-1.999.130
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 > /dev/null &' /etc/rc.local
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 > /dev/null &
+cd
+
+# permission
+chmod +x /usr/local/bin/badvpn-udpgw
+chmod +x /usr/local/share/man/man7/badvpn.7
+chmod +x /usr/local/bin/badvpn-tun2socks
+chmod +x /usr/local/share/man/man8/badvpn-tun2socks.8
+chmod +x /usr/bin/build
+chmod +x /etc/rc.local
+
+# Custom Banner SSH
+wget -O /etc/issue.net "https://gitlab.com/presult77/autoscript/-/raw/master/redssh.net"
+chmod +x /etc/issue.net
+echo "DROPBEAR_BANNER="/etc/issue.net"" >> /etc/default/dropbear
+
+# install fail2ban
+apt-get -y install fail2ban
+service fail2ban restart
+
+# Instal DDOS Flate
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+# download script
+cd /usr/bin
+wget -O menu "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/menu.sh"
+wget -O user-add "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/useradd.sh"
+wget -O trial "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/trial.sh"
+wget -O user-login "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/userlogin.sh"
+wget -O user-list "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/userlist.sh"
+wget -O fix "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/fix.sh"
+wget -O speedtest "https://raw.githubusercontent.com/idtunnel/sshtunnel/master/debian9/speedtest_cli.py"
+wget -O info "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/info.sh"
+wget -O user-del "https://gitlab.com/presult77/autoscript/raw/master/userdel.html"
+wget -O user-lock "https://gitlab.com/presult77/autoscript/raw/master/user-lock.html"
+wget -O user-expire "https://gitlab.com/presult77/autoscript/-/raw/master/debian10/userexpire.sh"
+
+chmod +x menu
+chmod +x user-add
+chmod +x trial
+chmod +x user-login
+chmod +x user-list
+chmod +x fix
+chmod +x speedtest
+chmod +x info
+chmod +x user-del
+chmod +x user-lock
+chmod +x user-expire
+
+# finishing
+cd
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+service squid restart
+rm -rf ~/.bash_history && history -c
+echo "unset HISTFILE" >> /etc/profile
+sed -i '$ i\0 */12 * * * root /usr/bin/user-expire' /etc/crontab
+sed -i '$ i\0 */12 * * * root /usr/bin/user-lock' /etc/crontab
+
+cd
+
+# Delete script
+rm -f /root/deb10.sh
