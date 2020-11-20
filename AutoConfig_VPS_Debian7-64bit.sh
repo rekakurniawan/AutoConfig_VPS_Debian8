b0366c0484835888ca92506dd531676030b52393
rekakurniawan <reka.kurniawan7@gmail.com>
2020-11-20 17:54:55 +0700

    Add files via upload

---
+++ AutoConfig_VPS_Debian7-64bit.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+#
+
+# Initializating var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# Go to root directory
+cd
+
+# Disabling IPv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# Installing wget and curl
+apt-get update;apt-get -y install wget curl;
+
+# Set Localtime
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# Set Locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+# Installing ca-certificates
+apt-get install ca-certificates
+
+# Adding Repositories
+wget "http://www.dotdeb.org/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
+
+# Update
+apt-get update
+
+# install webserver
+apt-get -y install nginx
+
+# install essential package
+apt-get -y install nano iptables dnsutils openvpn screen whois ngrep unzip unrar
+
+# disable exim
+service exim4 stop
+sysv-rc-conf exim4 off
+
+# update apt-file
+apt-file update
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+echo "clear" >> .bash_profile
+echo "neofetch" >> .bash_profile
+
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Welcome to Skyla Server</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/vps.conf"
+service nginx restart
+
+# install figlet
+apt-get install figlet
+echo "clear" >> .bashrc
+echo 'figlet -k "$HOSTNAME"' >> .bashrc
+echo 'echo -e "Selamat datang ke server $HOSTNAME"' >> .bashrc
+echo 'echo -e "Script mod by Aiman Amir"' >> .bashrc
+echo 'echo -e "Taip menu untuk menampilkan senarai perintah yang tersedia"' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+
+# install openvpn
+wget -O /etc/openvpn/openvpn.tar "https://github.com/rekakurniawan/VPS-Debian10/blob/main/vpn.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+rm -f /etc/openvpn/openvpn.tar
+wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/1194.conf"
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+iptables-save > /etc/iptables_set.conf
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/iptables"
+chmod +x /etc/network/if-up.d/iptables
+service openvpn restart
+
+# konfigurasi openvpn
+cd /etc/openvpn/
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/1194.conf"
+sed -i $MYIP2 /etc/openvpn/client.ovpn;
+cp client.ovpn /home/vps/public_html/
+
+# install badvpn
+cd
+wget -O /usr/bin/badvpn-udpgw "https://github.com/rekakurniawan/VPS-Debian10/raw/main/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://github.com/rekakurniawan/VPS-Debian10/raw/main/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+
+# setting port ssh
+cd
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+service ssh restart
+
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=444/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 444 -p 80"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
+
+# install stunnel4
+apt-get -y install stunnel4
+wget -O /etc/stunnel/stunnel.pem "https://raw.githubusercontent.com/kholizsivoi/stunnel4/master/stunnel.pem"
+wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/kholizsivoi/stunnel4/master/stunnel.conf"
+sed -i $MYIP2 /etc/stunnel/stunnel.conf
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart
+
+# install fail2ban
+apt-get -y install fail2ban;
+service fail2ban restart
+
+# install squid3
+cd
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
+
+# install webmin
+cd
+wget -O webmin-current.deb "http://www.webmin.com/download/deb/webmin-current.deb"
+dpkg -i --force-all webmin-current.deb;
+apt-get -y -f install;
+rm /root/webmin-current.deb
+service webmin restart
+
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/usernew.sh"
+wget -O trial "hhttps://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/hapus.sh"
+wget -O login "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/user-list.sh"
+wget -O resvis "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/resvis.sh"
+wget -O speedtest "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/rekakurniawan/VPS-Debian10/main/about.sh"
+echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
+echo "* * * * * service dropbear restart" > /etc/cron.d/dropbear
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x hapus
+chmod +x login
+chmod +x member
+chmod +x resvis
+chmod +x speedtest
+chmod +x about
+
+# finishing
+cd
+chown -R www-data:www-data /home/vps/public_html
+service nginx start
+service openvpn restart
+service cron restart
+service ssh restart
+service dropbear restart
+service squid3 restart
+service webmin restart
+rm -rf ~/.bash_history && history -c
+echo "unset HISTFILE" >> /etc/profile
+
+# info
+clear
+echo "Autoscript Include:" | tee log-install.txt
+echo "===========================================" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Service"  | tee -a log-install.txt
+echo "-------"  | tee -a log-install.txt
+echo "OpenSSH  : 22, 80"  | tee -a log-install.txt
+echo "Dropbear : 443, 143"  | tee -a log-install.txt
+echo "Squid3   : 8080, 3128 (limit to IP SSH)"  | tee -a log-install.txt
+echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:81/client.ovpn)"  | tee -a log-install.txt
+echo "badvpn   : badvpn-udpgw port 7300"  | tee -a log-install.txt
+echo "nginx    : 81"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Script"  | tee -a log-install.txt
+echo "------"  | tee -a log-install.txt
+echo "menu (Senarai perintah)"  | tee -a log-install.txt
+echo "usernew (Membuat Akaun SSH)"  | tee -a log-install.txt
+echo "trial (Membuat Akaun Trial)"  | tee -a log-install.txt
+echo "hapus (Menghapus Akaun SSH)"  | tee -a log-install.txt
+echo "login (Semak login user)"  | tee -a log-install.txt
+echo "member (Senarai user)"  | tee -a log-install.txt
+echo "resvis (Restart Service dropbear, webmin, squid3, openvpn dan ssh)"  | tee -a log-install.txt
+echo "reboot (Reboot VPS)"  | tee -a log-install.txt
+echo "speedtest (Speedtest VPS)"  | tee -a log-install.txt
+echo "about (Informasi tentang script auto install)"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Feature lain"  | tee -a log-install.txt
+echo "----------"  | tee -a log-install.txt
+echo "Webmin   : http://$MYIP:10000/"  | tee -a log-install.txt
+echo "Timezone : Asia/Malaysia (GMT +8)"  | tee -a log-install.txt
+echo "IPv6     : [off]"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Modified by Aiman Amir"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Log Instalasi --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "VPS AUTO REBOOT TIAP 12 JAM"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "==========================================="  | tee -a log-install.txt
+cd
